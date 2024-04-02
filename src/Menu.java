fcb4c6c8237a70f303d46dd331657a84096091b5
Gilberto Costa <gilinhojrc@gmail.com>
2024-04-02 22:43:54 +0100

    go
    go

---
+++ src/Menu.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Menu {
+    public static void main(String[] args) {
+
+
+        //Saudação
+        System.out.println("✋\uD83D\uDE03 OLÁ ✋\uD83D\uDE03 BEM-VINDO À GAMESTART!\uD83C\uDF89\nA tua loja de videojogos favorita!");
+        System.out.println("A tua loja");
+
+
+
+        //Importar o scanner
+        Scanner input = new Scanner(System.in);
+
+
+
+
+
+
+
+        // Declarar variáveis
+        String ADMIN, CLIENTE;
+    }
+}
